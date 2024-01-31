72b38670336f49e64d9fac5b383e7428e685e393
TheActualSanny <148360536+TheActualSanny@users.noreply.github.com>
2024-02-01 03:04:30 +0400

    Update main_parser.py

--- main_parser.py
+++ main_parser.py
@@ -1,64 +1,20 @@
-from pydantic import BaseModel, field_validator
 import requests
 import dotenv
 import os
-import csv
 import json
-import logging
+from residental_Data import Residental_Data
+from logging_Setup import parsing
+from csv_data import get_csv_Data
+from json_data import get_json_Data
 
 dotenv.load_dotenv()
 
-#setting up the logger for parsing.log
-parsing = logging.getLogger('exception')
-parsing.setLevel(logging.DEBUG)
-format1 = logging.Formatter('%(asctime)s - %(levelname)s - %(funcName)s - %(message)s')
-file1 = logging.FileHandler('parsing.log')
-file1.setFormatter(format1)
-parsing.addHandler(file1)
-
-
-#Initialized another logger for connection_statuses.log
-conn = logging.getLogger('connection_report')
-conn.setLevel(logging.INFO)
-format2 = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file2 = logging.FileHandler('connection_statuses.log')
-file2.setFormatter(format2)
-conn.addHandler(file2)
-
-
-class Residental_Data(BaseModel):
-    residental_area: str
-    stated_year: int
-    below_6: int
-    from_6_to_10: int
-    from_10_to_18: int
-    from_18_to_25: int
-    from_25_to_30: int
-    from_30_to_50: int
-    from_50_to_65: int
-    from_65_to_75: int
-    older_than_75: int
-    
-    @field_validator('from_10_to_18', 'from_6_to_10', 'from_10_to_18', 
-                     'from_18_to_25', 'from_25_to_30', 'from_30_to_50',
-                     'from_50_to_65', 'from_65_to_75', 'older_than_75',
-                     'below_6', mode = 'before')
-    @classmethod
-    def check_below_6(cls, val):
-        if val == '.' or not val:
-            parsing.debug('Caught an exception! modified the value...')
-            return 0
-        else:
-            return int(val)
-        
-
 payload = {}
 headers = {'User-Agent' : os.getenv('AGENT_DATA')}
-dataset = "https://data.europa.eu/api/hub/search/datasets/1a57a33d-3a99-4fbc-b56f-b4b109172c7d"
-response = requests.request("GET", dataset, headers=headers, data=payload)
-dataset_json = json.loads(response.text)
-
-year_2015_url = dataset_json['result']['distributions'][0]['download_url']
+datasets = "https://data.europa.eu/api/hub/search/datasets/1a57a33d-3a99-4fbc-b56f-b4b109172c7d"
+response = requests.request("GET", datasets, headers=headers, data=payload)
+datasets_json = json.loads(response.text)
+year_2015_url = datasets_json['result']['distributions'][0]['download_url']
 
 url_csv = year_2015_url[0]
 url_json_first = 'https://opendata.duesseldorf.de/api/action/datastore/search.json?resource_id=99ff11f6-ddfd-4698-86e9-453ce2ce859a'
@@ -66,49 +22,15 @@ url_json_second = "https://www.opendata.duesseldorf.de/api/action/datastore/sear
 # Will contain all of the instances of residental_data class and will be used in DB creation.
 finalized_data = []
 
-response1 = requests.request("GET", url_csv, headers=headers, data=payload)
-response2 = requests.request('GET', url_json_first, headers = headers, data = payload)
-response3 = requests.request('GET', url_json_second, headers = headers, data = payload)
 
-if not all([i == 200 for i in [response1.status_code, response2.status_code, response3.status_code]]):
-    conn.error('Connection error occured. Make sure to provide the headers to the API...')
-    raise ConnectionError('Connection error occured. Make sure to provide the headers to the API...')
+def main():
+    global finalized_data
+    finalized_data += get_csv_Data(url_csv) + get_json_Data(url_json_first, url_json_second)
+    return finalized_data
 
-else:
-    conn.info('Connected to all 3 API-s successfully!')
-    with open('2015_dusseldorf_data.csv', 'w') as f:
-        f.write(response1.text)
+call = main()
 
-    with open('2015_dusseldorf_data.csv', 'r') as f:
-        csv_reader = csv.DictReader(f, delimiter = ';')
-        for residental_area in csv_reader:
-            values = list(residental_area.values())
-            if values.count('0') < 9:
-                instance = Residental_Data(residental_area=values[0], stated_year=f.name[0:4],
-                                        from_6_to_10=values[2], from_10_to_18=values[3],
-                                        from_18_to_25=values[4], from_25_to_30=values[5],
-                                        from_30_to_50=values[6], from_50_to_65=values[7],
-                                        from_65_to_75=values[8], older_than_75=values[9], 
-                                        below_6=values[1])
-                finalized_data.append(instance)
 
-    for iter, data in enumerate([response2.text, response3.text], start = 1):
-        json_data = json.loads(data)
-        if iter == 1:
-            upload_year = '2016'
-        else:
-            upload_year = '2017'     
-        for record in json_data['result']['records']:
-            values = list(record.values())
-            if values.count('0') < 9:
-                instance = Residental_Data(residental_area=values[0], stated_year=upload_year,
-                                            from_6_to_10=values[2], from_10_to_18=values[3],
-                                            from_18_to_25=values[4], from_25_to_30=values[5],
-                                            from_30_to_50=values[6], from_50_to_65=values[7],
-                                            from_65_to_75=values[8], older_than_75=values[9], 
-                                            below_6=values[1])
-                finalized_data.append(instance)
-
-    if __name__ == '__main__':
-        for instance in finalized_data:
-            print(instance.residental_area, end = '\n')
+if __name__ == '__main__':
+    for instance in finalized_data:
+        print(instance.residental_area, end = '\n')
